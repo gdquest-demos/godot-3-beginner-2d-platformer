ef2964bc2b2b328775bda2bbd78e3b74c7e03fc5
Henrique Campos <henriquecampos@pigdev.studio>
2019-08-28 18:13:19 -0300

    Add BumpArea2D to enemy

--- game/src/actors/Enemy.gd
+++ game/src/actors/Enemy.gd
@@ -16,3 +16,7 @@ func get_hurt() -> void:
 func _on_StompArea2D_body_entered(body: PhysicsBody2D) -> void:
 	if body.is_in_group("player"):
 		get_hurt()
+
+
+func _on_BumpArea2D_body_shape_entered(body_id: int, body: PhysicsBody2D, body_shape: int, area_shape: int) -> void:
+	_linear_velocity.x = speed.x if area_shape == 0 else -speed.x
