09aa2187b0a809f487a0f3daf49252b74871ae2e
Nathan Lovato <nathan@gdquest.com>
2019-09-08 10:43:08 +0200

    Use animation player for the coin instead of tween code

--- game/src/objects/Coin.gd
+++ game/src/objects/Coin.gd
@@ -1,27 +1,16 @@
 extends Area2D
 
 
-export var score: = 100
+onready var anim_player: AnimationPlayer = $AnimationPlayer
 
-const TWEEN_FINAL_POSITION = Vector2(0, -50)
-const TWEEN_DURATION = 0.25
+export var score: = 100
 
 
 func _on_body_entered(body: PhysicsBody2D) -> void:
 	if body.is_in_group("player"):
-		get_picked()
+		picked()
 
 
-func get_picked() -> void:
+func picked() -> void:
 	PlayerData.score += score
-	
-	$Tween.interpolate_property(self, "position", position,
-			position + TWEEN_FINAL_POSITION, TWEEN_DURATION, Tween.TRANS_BACK, Tween.EASE_IN)
-	var transparent_color: = modulate
-	transparent_color.a = 0.0
-	$Tween.interpolate_property(self, "modulate", modulate,
-			transparent_color, TWEEN_DURATION, Tween.TRANS_LINEAR, Tween.EASE_IN)
-	$Tween.start()
-	
-	yield($Tween, "tween_all_completed")
-	queue_free()
+	anim_player.play("picked")
