5f3cd41f765c798d374b60d0ea39890f7ca533cf
Nathan Lovato <nathan@gdquest.com>
2020-02-26 07:49:38 -0600

    Pause the game when entering a portal
    Fixes #35

--- game/src/Objects/Portal2D.gd
+++ game/src/Objects/Portal2D.gd
@@ -16,6 +16,8 @@ func _get_configuration_warning() -> String:
 
 
 func teleport() -> void:
+	get_tree().paused = true
 	anim_player.play("fade_out")
 	yield(anim_player, "animation_finished")
+	get_tree().paused = false
 	get_tree().change_scene_to(next_scene)
